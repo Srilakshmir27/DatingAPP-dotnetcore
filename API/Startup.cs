6f7a8704681251b823c1abccdc948ef12310359f
LAPTOP-RICKY\vicky <brajput@vcidata.com>
2021-11-17 13:36:50 +0530

    end of section3

--- API/Startup.cs
+++ API/Startup.cs
@@ -26,6 +26,7 @@ namespace API
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "WebAPIv5", Version = "v1" });
             });
+            services.AddCors();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -42,6 +43,7 @@ namespace API
 
             app.UseRouting();
 
+            app.UseCors(policy=>policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:4200"));
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
